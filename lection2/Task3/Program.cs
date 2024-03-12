5d43253c41b32081d0dc56fe4e3a76fe63c3c060
Александр <faqyou72@gmail.com>
2024-03-12 22:38:00 +0500

    добавили Task4

---
+++ lection2/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿
+int[] arr = { -2, -5, 4, -7, 8, 5, 6, 2, 3, 10 };
+int i = 0;
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
